9daf5594b8f2a9e8f57b7050c140ce1508183bf1
Toshiyuki Masui <masui@pitecan.com>
2016-07-24 19:27:46 +0100

    READMEとxpiを追加

---
+++ README.md
@@ -0,0 +1,9 @@
+# GyazzSlide
+
+* Gyazzページをスライドショー表示するブラウザ拡張機能です
+* FirefoxとChromeで動きます
+
+## 使い方
+
+* Chromeの拡張機能メニューから「パッケージ化されてない拡張機能を読み込む」を選び、GyazzSlideフォルダを指定する
+* Firefoxの```about:addons```のメニューから「ファイルから機アドオンをインストール」を選び、gyazoslide.xpiを選択する
--- slide.js
+++ slide.js
@@ -1,6 +1,8 @@
 //
 // 本体JS
 //
+// shokaiのGyazzACloneでやってる処理をここで書くべきなのだろうけど
+//
 chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
     var a = document.createElement("script");
     a.setAttribute("src","//shokai.github.io/GyazzAClone/dist/index.js?time="+Date.now());
