97dad3db9c1f16cb37a0a2c7629232e7aba76792
Toshiyuki Masui <masui@pitecan.com>
2016-07-24 17:48:53 +0100

    ...

--- slide.js
+++ slide.js
@@ -1,16 +1,8 @@
-// alert("SLIDE.js");
 //
-//alert(chrome.extension.onRequest.addListener);
+// 本体JS
 //
-// chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
-
 chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
-    //alert("req from BG!!! Do whatever we want!");
-    // alert(document);
     var a = document.createElement("script");
     a.setAttribute("src","//shokai.github.io/GyazzAClone/dist/index.js?time="+Date.now());
     document.querySelector("body").appendChild(a);
-    //alert(tab.title);
 });
-
-// alert("SLIDE.js end");
--- slide_bg.js
+++ slide_bg.js
@@ -1,25 +1,9 @@
-//alert(1000);
-
+//
+// Background JS
+// 
+// よくわからないが、アイコンクリックするとこれが呼ばれるので仕方なく
+//
 chrome.browserAction.onClicked.addListener(function(tab) {
-    //alert(tab);
-    //alert(tab.id);
-
-    //sendResponse({tabid:tab.id});
-    //alert(chrome.runtime.sendMessage);
-
-    //chrome.runtime.sendMessage(tab.id, {}, {}, function(){
-
     chrome.tabs.sendMessage(tab.id, {}, {}, function(){
-	// alert("sendMessage end");
     });
-
-    // alert("xxxxx");
-
-    //var a = tab.createElement("script");
-    //a.setAttribute("src","//shokai.github.io/GyazzAClone/dist/index.js?time="+Date.now());
-    //tab.querySelector("body").appendChild(a);
-    //alert(tab.title);
-    //chrome.tabs.executeScript({
-    //  code: 'alert(100)'
-    //});
 });
